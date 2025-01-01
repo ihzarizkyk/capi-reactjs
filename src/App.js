f4008321ee204048989fc1a51d62d263f7b47055
ihzarizkyk <ihzarizky30@gmail.com>
2025-01-01 17:36:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function Button(){
+  return(
+    <button type="button" className="welcome">
+    Consume API with react
+    </button>
+    );
+}
+
+export default function App(){
+  return(
+      <Button/>
+    );
+}
